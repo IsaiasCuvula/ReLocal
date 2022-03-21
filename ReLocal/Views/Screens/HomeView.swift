a37a8f892d1ff23b58e0c753f2b6a8334a5ae465
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2022-03-21 23:28:13 +0200

    some screens added

---
+++ ReLocal/Views/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  ReLocal
+//
+//  Created by Isaias Cuvula on 21.03.22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
